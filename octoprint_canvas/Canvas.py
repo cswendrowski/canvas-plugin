7ea7d2411c15080717ff28ee26a3411ea54b88b1
Khoi <hakhoipham@gmail>
2019-01-22 09:52:23 -0500

    2.0.0 added and additional check for upgraded hubs

--- octoprint_canvas/Canvas.py
+++ octoprint_canvas/Canvas.py
@@ -46,7 +46,7 @@ class Canvas():
     ##############
 
     def checkFor0cf0(self):
-        if os.path.isdir("/home/pi/.mosaicdata/turquoise/") and self.hub_yaml["canvas-hub"]["hub"]["name"] == "0cf0-ch" and self.hub_yaml["canvas-hub"]["hub"]["id"] == "46f352c67dd7bc1e5a28b66cf960290d" and self.hub_yaml["canvas-hub"]["token"] == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NDIzODIxMTQsImlzcyI6IkNhbnZhc0h1YiIsInN1YiI6IjQ2ZjM1MmM2N2RkN2JjMWU1YTI4YjY2Y2Y5NjAyOTBkIn0.CMDTVKAuI2USNwvx1gjKVBMgTRCnOX8WBhp2XTjjhLM":
+        if os.path.isdir("/home/pi/.mosaicdata/turquoise/") and "version" not in self.hub_yaml["canvas-hub"] and self.hub_yaml["canvas-hub"]["hub"]["name"] == "0cf0-ch" and self.hub_yaml["canvas-hub"]["hub"]["id"] == "46f352c67dd7bc1e5a28b66cf960290d" and self.hub_yaml["canvas-hub"]["token"] == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NDIzODIxMTQsImlzcyI6IkNhbnZhc0h1YiIsInN1YiI6IjQ2ZjM1MmM2N2RkN2JjMWU1YTI4YjY2Y2Y5NjAyOTBkIn0.CMDTVKAuI2USNwvx1gjKVBMgTRCnOX8WBhp2XTjjhLM":
             self._logger.info("0cf0 found.")
             del self.hub_yaml["canvas-hub"]["hub"]
             del self.hub_yaml["canvas-hub"]["token"]
