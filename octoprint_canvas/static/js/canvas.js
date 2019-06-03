cfd5f0374bf4cbb017f433162b8e2580e097daea
Khoi <hakhoipham@gmail>
2019-06-03 12:02:27 -0400

    made UI functions into its separate utils file

--- octoprint_canvas/static/js/canvas.js
+++ octoprint_canvas/static/js/canvas.js
@@ -1,186 +1,3 @@
-if (!document.getElementById("material-icons")) {
-  let link = document.createElement("link");
-  link.id = "material-icons";
-  link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
-  link.rel = "stylesheet";
-  document.head.appendChild(link);
-}
-if (!document.getElementById("sweetalert2-styling")) {
-  let link = document.createElement("link");
-  link.id = "sweetalert2-styling";
-  link.href = "https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.29.0/sweetalert2.min.css";
-  link.rel = "stylesheet";
-  document.head.appendChild(link);
-}
-if (!document.getElementById("sweetalert2-script")) {
-  let script = document.createElement("script");
-  script.id = "sweetalert2-script";
-  script.src = "https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.29.0/sweetalert2.min.js";
-  document.head.appendChild(script);
-}
-
-const canvasApp = {};
-
-/* ======================
-  CANVAS THEME FUNCTIONALITIES
-  ======================= */
-
-/* 1. Replaces Octoprint Logo with Mosaic */
-canvasApp.toggleLogo = condition => {
-  if (condition) {
-    $("head")
-      .find('link[rel="shortcut icon"]')
-      .attr("href", "/plugin/canvas/static/img/Mosaic_Icon_Square.png");
-  } else {
-    $("head")
-      .find('link[rel="shortcut icon"]')
-      .attr("href", "/static/img/tentacle-20x20@2x.png");
-  }
-};
-
-/* 2. Display popup notifications for files incoming and received from Canvas */
-canvasApp.displayNotification = data => {
-  // if a prior popup with the same file is currently on the page, remove it
-  if ($("body").find(`#${data.projectId}`).length > 0) {
-    $("body")
-      .find(`#${data.projectId}`)
-      .fadeOut(1000, function () {
-        this.remove();
-      });
-  }
-  let notification = $(`<li id="${data.projectId}" class="canvas-progress-bar popup-notification">
-            <i class="material-icons remove-popup">clear</i>
-            <div class="popup-heading">
-              <h6 class="popup-title">CANVAS File Incoming...</h6>
-              <div class="small-loader"></div>
-            </div>
-            <p class="file-download-name">${data.filename}</p>
-            <div class="total-bar">
-              <div class="current-bar">
-                <div class="progression-tool-tip"></div>
-                <span>&nbsp;</span>
-              </div>
-            </div>
-            </li>`).hide();
-  $(".side-notifications-list").append(notification);
-  notification.fadeIn(200);
-};
-
-/* 2.1 Update the download progress (%) on UI */
-canvasApp.updateDownloadProgress = data => {
-  if (data.current === 0) {
-    $("body")
-      .find(`#${data.projectId} .total-bar`)
-      .css("position", "static");
-    $("body")
-      .find(`#${data.projectId} .current-bar`)
-      .css("position", "relative");
-    $("body")
-      .find(`#${data.projectId} .progression-tool-tip`)
-      .css({ left: "0%", right: "auto", visibility: "visible" })
-      .removeClass("tool-tip-arrow");
-  } else if (data.current === 10) {
-    $("body")
-      .find(`#${data.projectId} .progression-tool-tip`)
-      .css({ left: "auto", right: "-13.5px" })
-      .addClass("tool-tip-arrow");
-  } else if (data.current === 96) {
-    $("body")
-      .find(`#${data.projectId} .total-bar`)
-      .css("position", "relative");
-    $("body")
-      .find(`#${data.projectId} .current-bar`)
-      .css("position", "static");
-    $("body")
-      .find(`#${data.projectId} .progression-tool-tip`)
-      .css({ left: "auto", right: "0%" })
-      .removeClass("tool-tip-arrow");
-  }
-  $("body")
-    .find(`#${data.projectId} .current-bar`)
-    .css("width", data.current + "%");
-  $("body")
-    .find(`#${data.projectId} .progression-tool-tip`)
-    .text(data.current + "%");
-};
-
-/* 2.2 Update download progress when file is received and extracted */
-canvasApp.updateFileReceived = data => {
-  $("body")
-    .find(`#${data.projectId} .popup-title`)
-    .text("File Received. Please Wait...")
-    .hide()
-    .fadeIn(200);
-  $("body")
-    .find(`#${data.projectId} .small-loader`)
-    .fadeIn(200);
-  $("body")
-    .find(`#${data.projectId} .total-bar`)
-    .fadeOut(200);
-  $("body")
-    .find(`#${data.projectId} .file-download-name`)
-    .text(data.filename)
-    .hide()
-    .fadeIn(200);
-};
-
-/* 2.3 Update download progress when file analysis is done */
-canvasApp.updateFileReady = filename => {
-  $("body")
-    .find(`.canvas-progress-bar .file-download-name:contains("${filename}")`)
-    .siblings(".popup-heading")
-    .children(".popup-title")
-    .text("CANVAS File Added")
-    .hide()
-    .fadeIn(200);
-  $("body")
-    .find(`.canvas-progress-bar .file-download-name:contains("${filename}")`)
-    .siblings(".popup-heading")
-    .children(".small-loader")
-    .remove();
-  setTimeout(function () {
-    $("body")
-      .find(`.canvas-progress-bar .file-download-name:contains("${filename}")`)
-      .closest("li")
-      .addClass("highlight-glow-received");
-  }, 400);
-};
-
-/* 3. Remove popup notifications */
-canvasApp.removePopup = () => {
-  $("body").on("click", ".side-notifications-list .remove-popup", function () {
-    $(this)
-      .closest("li")
-      .fadeOut(200, function () {
-        $(this).remove();
-      });
-  });
-};
-
-/* 4. Loader */
-canvasApp.loadingOverlay = condition => {
-  if (condition) {
-    $("body").append(`<div class="loading-overlay-container"><div class="loader"></div></div>`);
-  } else {
-    $("body")
-      .find(".loading-overlay-container")
-      .remove();
-  }
-};
-
-/* 5. Add Notification List To DOM */
-canvasApp.addNotificationList = () => {
-  if ($("body").find(".side-notifications-list").length === 0) {
-    $("body")
-      .css("position", "relative")
-      .append(`<ul class="side-notifications-list"></ul>`);
-  }
-};
-
-/* ======================
-  CANVAS VIEW MODEL FOR OCTOPRINT
-  ======================= */
-
 function CanvasViewModel(parameters) {
   var self = this;
 
@@ -286,19 +103,19 @@ function CanvasViewModel(parameters) {
   };
 
   self.onStartupComplete = () => {
-    canvasApp.removePopup();
-    canvasApp.addNotificationList();
+    UI.removePopup();
+    UI.addNotificationList();
   };
 
   self.onEventFileAdded = payload => {
     if ($("body").find(`.canvas-progress-bar .file-download-name:contains("${payload.name}")`)) {
-      canvasApp.updateFileReady(payload.name);
+      UI.updateFileReady(payload.name);
     }
   };
 
   self.onDataUpdaterReconnect = () => {
-    canvasApp.removePopup();
-    canvasApp.addNotificationList();
+    UI.removePopup();
+    UI.addNotificationList();
   };
 
   self.onEventConnected = () => {
@@ -306,7 +123,7 @@ function CanvasViewModel(parameters) {
   };
 
   self.addUser = () => {
-    canvasApp.loadingOverlay(true);
+    UI.loadingOverlay(true);
     let payload = { command: "addUser", data: { username: self.userInput(), password: self.password() } };
 
     if (self.userInput().includes("@")) {
@@ -353,11 +170,11 @@ function CanvasViewModel(parameters) {
     if (applyTheme) {
       self.applyTheme(true);
       $("html").addClass("canvas-theme");
-      canvasApp.toggleLogo(applyTheme);
+      UI.toggleLogo(applyTheme);
     } else {
       self.applyTheme(false);
       $("html").removeClass("canvas-theme");
-      canvasApp.toggleLogo(applyTheme);
+      UI.toggleLogo(applyTheme);
     }
 
     // Event listener for when user changes the theme settings
@@ -367,11 +184,11 @@ function CanvasViewModel(parameters) {
       if (applyTheme) {
         self.applyTheme(true);
         $("html").addClass("canvas-theme");
-        canvasApp.toggleLogo(applyTheme);
+        UI.toggleLogo(applyTheme);
       } else {
         self.applyTheme(false);
         $("html").removeClass("canvas-theme");
-        canvasApp.toggleLogo(applyTheme);
+        UI.toggleLogo(applyTheme);
       }
     });
   };
@@ -405,25 +222,25 @@ function CanvasViewModel(parameters) {
         self.handleAWSConnection(message);
       } else if (message.command === "UserConnectedToHUB") {
         $(".add-user input").val("");
-        canvasApp.loadingOverlay(false);
+        UI.loadingOverlay(false);
         Alerts.userAddedSuccess(message.data.username);
       } else if (message.command === "UserAlreadyExists") {
         $(".add-user input").val("");
-        canvasApp.loadingOverlay(false);
+        UI.loadingOverlay(false);
         Alerts.userExistsAlready(message.data.username);
       } else if (message.command === "invalidUserCredentials") {
         $(".add-user input").val("");
-        canvasApp.loadingOverlay(false);
+        UI.loadingOverlay(false);
         Alerts.userInvalidCredentials();
       } else if (message.command === "UserDeleted") {
         Alerts.userDeletedSuccess(message.data);
       } else if (message.command === "CANVASDownload") {
         if (message.status === "starting") {
-          canvasApp.displayNotification(message.data);
+          UI.displayNotification(message.data);
         } else if (message.status === "downloading") {
-          canvasApp.updateDownloadProgress(message.data);
+          UI.updateDownloadProgress(message.data);
         } else if (message.status === "received") {
-          canvasApp.updateFileReceived(message.data);
+          UI.updateFileReceived(message.data);
         }
       } else if (message.command === "importantUpdate") {
         $("body").on("click", ".update-checkbox input", event => {
--- octoprint_canvas/static/js/utils/alerts.js
+++ octoprint_canvas/static/js/utils/alerts.js
@@ -1,3 +1,17 @@
+if (!document.getElementById("sweetalert2-styling")) {
+  let link = document.createElement("link");
+  link.id = "sweetalert2-styling";
+  link.href = "https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.29.0/sweetalert2.min.css";
+  link.rel = "stylesheet";
+  document.head.appendChild(link);
+}
+if (!document.getElementById("sweetalert2-script")) {
+  let script = document.createElement("script");
+  script.id = "sweetalert2-script";
+  script.src = "https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.29.0/sweetalert2.min.js";
+  document.head.appendChild(script);
+}
+
 const Alerts = {
   userAddedSuccess: username => {
     return swal({
