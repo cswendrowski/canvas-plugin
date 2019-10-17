3d19d9b0cf123e04cccbf4b8033e058adf3f3fd0
Khoi <hakhoipham@gmail>
2019-10-17 14:27:38 -0400

    Revert "Revert "send in optional registration property""
    This reverts commit e082bea96bbcf53a80489865fb1ddadef8ff7794.

--- octoprint_canvas/Canvas.py
+++ octoprint_canvas/Canvas.py
@@ -40,6 +40,7 @@ class Canvas():
         self.hub_registered = False
 
         self.hub_yaml = {}
+        self.isHubS = self.determineHubVersion()
         self.registerThread = None
 
     ##############
@@ -133,6 +134,8 @@ class Canvas():
             hostname = self.getHostname()
             if hostname:
                 payload["hostname"] = hostname
+            if self.isHubS:
+                payload["isHubS"] = True
 
             url = "https://" + BASE_URL_API + "hubs"
             try:
@@ -186,6 +189,19 @@ class Canvas():
             updated = True
         if updated:
             self.updateYAMLInfo()
+
+    def determineHubVersion(self):
+        hub_file_path = os.path.expanduser('~') + "/.mosaicdata/canvas-hub-data.yml"
+
+        if os.path.exists(hub_file_path):
+            hub_data = open(hub_file_path, "r")
+            hub_yaml = yaml.load(hub_data)
+            hub_data.close()
+
+            hub_rank = hub_yaml["versions"]["global"]
+            if hub_rank == "0.2.0":
+                return True
+        return False
 
     def startRegisterThread(self):
         if self.registerThread is None:
